311e542ba26f672ffd8bfd05b03a2c7bc7c6233a
Wanty Jean-François <wanty.jf@gmail.com>
2024-03-27 11:26:04 +0100

    Create the recipes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RecipeModule } from './recipe/recipe.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [RecipeModule],
+  imports: [RecipesModule],
   controllers: [AppController],
   providers: [AppService],
 })
